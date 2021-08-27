3c62862f842189fa9e7871141dbdbe3c7d25e87b
kev-inc <kevinchanxy@gmail.com>
2021-08-27 03:08:48 +0000

    fix bug

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonButton } from '@ionic/react';
 import cheerio from 'cheerio'
