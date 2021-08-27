88c00ff65481db74e061f829208da1097eb3ee45
kev-inc <kevinchanxy@gmail.com>
2021-08-27 03:51:36 +0000

    added video metadata

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -10,10 +10,15 @@ const Tab1: React.FC = (props) => {
     const link = params.get('link') || ''
     const [videoId, setVideoId] = useState(link)
     const [audioSrc, setAudioSrc] = useState('')
+    const [videoMetadata, setVideoMetadata] = useState({})
 
 
     const retrieveMp3 = () => {
         setAudioSrc('')
+        const youtube = require('youtube-metadata-from-url');
+        youtube.metadata(videoId).then(json => {
+            setVideoMetadata(json)
+        })
         const id = videoId.split('/')[3]
         fetch("https://www.yt-download.org/api/button/mp3/" + id)
             .then(resp => resp.text())
@@ -32,7 +37,7 @@ const Tab1: React.FC = (props) => {
                     <IonTitle>YouTube Music</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent  class="ion-text-center" fullscreen>
+            <IonContent class="ion-text-center" fullscreen>
                 <IonHeader collapse="condense">
                     <IonToolbar>
                         <IonTitle size="large">Tab 1</IonTitle>
@@ -42,11 +47,13 @@ const Tab1: React.FC = (props) => {
                     <IonLabel position="stacked">YouTube Link</IonLabel>
                     <IonInput value={videoId} placeholder="Enter YouTube Link" onIonChange={e => setVideoId(e.detail.value!)}></IonInput>
 
-                </IonItem><IonButton expand="block" onClick={retrieveMp3}>Play!</IonButton>
+                </IonItem>
+                <IonButton expand="block" onClick={retrieveMp3}>Play!</IonButton>
 
+                <img src={videoMetadata['thumbnail_url']} width={videoMetadata['thumbnail_width']} alt='thumbnail'/>
+                <h3>{videoMetadata['title']}</h3>
                 {audioSrc.length > 0 && (
-
-                    <audio controls>
+                    <audio controls autoPlay>
                         <source type="audio/mpeg" src={audioSrc} />
                     </audio>
                 )}
