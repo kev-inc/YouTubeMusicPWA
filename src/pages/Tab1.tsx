58101e5cb98e22a31072a897fea2094b3351a9d2
kev-inc <kevinchanxy@gmail.com>
2021-08-27 17:38:53 +0000

    fix errors

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,28 +7,29 @@ const Tab1: React.FC = (props) => {
     const location = useLocation()
     const params = new URLSearchParams(location.search)
     const link = params.get('link') || ''
-    const [videoId, setVideoId] = useState(link)
+    const [videoId] = useState(link)
     const [audioSrc, setAudioSrc] = useState('')
     const [videoMetadata, setVideoMetadata] = useState({})
 
     useEffect(() => {
+        const retrieveMp3 = () => {
+            setAudioSrc('')
+            const youtube = require('youtube-metadata-from-url');
+            youtube.metadata(videoId).then(json => {
+                setVideoMetadata(json)
+            })
+            const id = videoId.split('/')[3]
+            fetch("https://py-youtube-dl.vercel.app/api?id=" + id)
+                .then(resp => resp.json())
+                .then(json => setAudioSrc(json['link']))
+        }
         if (videoId.length > 0) {
             retrieveMp3()
         }
     }, [videoId])
 
 
-    const retrieveMp3 = () => {
-        setAudioSrc('')
-        const youtube = require('youtube-metadata-from-url');
-        youtube.metadata(videoId).then(json => {
-            setVideoMetadata(json)
-        })
-        const id = videoId.split('/')[3]
-        fetch("https://py-youtube-dl.vercel.app/api?id=" + id)
-            .then(resp => resp.json())
-            .then(json => setAudioSrc(json['link']))
-    }
+    
 
     const onPlay = () => {
     }
